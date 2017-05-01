ea6710294329e0c118b87cb3e1ea9181de9e138c
T3mplarSw0rd <t3mplarsw0rd@gmail.com>
2017-04-30 19:06:25 -0500

    Potential Start

--- Python Final.py
+++ Python Final.py
@@ -1,5 +1,19 @@
 ########################################################
 # Names : Parker Sikes, Christian Thibodeaux, Marcus Castille
 ########################################################
-
+from random import randint
 #Logic Gates into Caesarian Cypher
+solutionInput = "0000"
+
+def Gates(self, previous = True):
+
+    Answer = ""
+    Correct = False
+
+GatesList = [G1, G2, G3, G4]
+for i in range (len(GatesList)):
+        if i == 0:
+            GatesList[i] = Gates()
+        else:
+            GatesList[i] = Gates(False)
+        i.Answer = str(bin.randint(0,16))
