752ae89ab1570e891b6abd893d4203e415289250
T3mplarSw0rd <t3mplarsw0rd@gmail.com>
2017-04-30 19:14:47 -0500

    Comments on plan

--- Python Final.py
+++ Python Final.py
@@ -5,15 +5,22 @@ from random import randint
 #Logic Gates into Caesarian Cypher
 solutionInput = "0000"
 
-def Gates(self, previous = True):
+def Gates(self):
 
     Answer = ""
     Correct = False
 
 GatesList = [G1, G2, G3, G4]
 for i in range (len(GatesList)):
-        if i == 0:
-            GatesList[i] = Gates()
-        else:
-            GatesList[i] = Gates(False)
+        GatesList[i] = Gates()
         i.Answer = str(bin.randint(0,16))
+
+
+"""
+Paper Piano Record function will be nice to move into this project
+due to its ability to toggle an input.
+Room Project's GUI will also be nice to cannibalize with the right side
+showing the current inputs being made.
+The left side showing which of the 4 gates have been solved and when complete.
+The right side will hold the shifted text for them to solve.
+"""
